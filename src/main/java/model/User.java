404e0886a30b3fe2a7ce4bcd40e55eba4c8c9316
PavelOkhrimchuk <okhrimchuk.pavel2005@gmail.com>
2024-08-13 21:13:54 +0300

    feat: add User model

---
+++ src/main/java/model/User.java
@@ -0,0 +1,32 @@
+package model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+
+    @Column(nullable = false, unique = true)
+    private String login;
+
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Location> locations = new HashSet<>();
+}
