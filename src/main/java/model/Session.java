142f7ac30544e1270738f513a789ed87a6e3e5a2
PavelOkhrimchuk <okhrimchuk.pavel2005@gmail.com>
2024-08-20 18:01:37 +0300

    refactor: changed lazy to eager

--- src/main/java/model/Session.java
+++ src/main/java/model/Session.java
@@ -22,7 +22,7 @@ public class Session {
     private UUID id;
 
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
