99d21797dc4845eeec5a18c3510fc49c24b08dcc
PavelOkhrimchuk <okhrimchuk.pavel2005@gmail.com>
2024-08-20 19:10:44 +0300

    feat: TestHibernateUtil implementation

---
+++ src/test/java/TestHibernateUtil.java
@@ -0,0 +1,38 @@
+import model.Location;
+import model.Session;
+import model.User;
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class TestHibernateUtil {
+
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+
+            Configuration configuration = new Configuration().configure("hibernate-test.cfg.xml");
+
+
+            configuration.addAnnotatedClass(User.class);
+            configuration.addAnnotatedClass(Location.class);
+            configuration.addAnnotatedClass(Session.class);
+
+
+            return configuration.buildSessionFactory();
+
+        } catch (HibernateException ex) {
+            System.err.println("Initial SessionFactory creation failed. " + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        getSessionFactory().close();
+    }
+}
