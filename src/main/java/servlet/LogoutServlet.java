d7173e74615a37d4fa592e0f7cd964d42054f2a0
PavelOkhrimchuk <okhrimchuk.pavel2005@gmail.com>
2024-08-20 18:27:37 +0300

    refactor: logout servlet changed

--- src/main/java/servlet/LogoutServlet.java
+++ src/main/java/servlet/LogoutServlet.java
@@ -6,21 +6,34 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import repository.SessionRepository;
+import service.SessionService;
+import util.HibernateUtil;
 
 import java.io.IOException;
+import java.util.UUID;
 
 @WebServlet("/logout")
 public class LogoutServlet extends BaseServlet{
 
+    private SessionService sessionService;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        this.sessionService = new SessionService(new SessionRepository(HibernateUtil.getSessionFactory()));
+    }
+
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        HttpSession session = req.getSession(false);
-        if(session!=null) {
-            session.invalidate();
-        }
+        String sessionIdStr = getCookieValue(req, "JSESSIONID");
 
-        removeCookie(resp, "JSESSIONID");
+        if (sessionIdStr != null) {
+            UUID sessionId = UUID.fromString(sessionIdStr);
+            sessionService.invalidateSession(sessionId);
+            removeCookie(resp, "JSESSIONID");
+        }
 
         resp.sendRedirect(req.getContextPath() + "/login");
     }
