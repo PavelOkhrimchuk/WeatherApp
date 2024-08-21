8c475b03758dcdba9737711296751f12cd900d9e
PavelOkhrimchuk <okhrimchuk.pavel2005@gmail.com>
2024-08-21 21:31:09 +0300

    feat: added methods WeatherService

--- src/main/java/service/WeatherService.java
+++ src/main/java/service/WeatherService.java
@@ -1,6 +1,70 @@
 package service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dto.WeatherResponseDto;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Optional;
+
 public class WeatherService {
 
+    private static final String API_KEY = "700daeb1e0b92b52a99f2c05f092db91";
+    private static final String BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
 
+    private final HttpClient httpClient;
+    private final ObjectMapper objectMapper;
+
+
+    public WeatherService() {
+        this.httpClient = HttpClient.newHttpClient();
+        this.objectMapper = new ObjectMapper();
+    }
+    public Optional<WeatherResponseDto> getWeatherByCity(String cityName) {
+        HttpRequest request = buildRequestByCity(cityName);
+        return sendRequest(request);
+    }
+
+    private HttpRequest buildRequestByCity(String cityName) {
+        String url = String.format("%s?q=%s&appid=%s",BASE_URL, cityName, API_KEY);
+        return HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .build();
+    }
+
+
+    public Optional<WeatherResponseDto> getWeatherByCoordinates(double lat, double lon) {
+        HttpRequest request = buildRequestByCoordinates(lat, lon);
+        return sendRequest(request);
+    }
+
+
+    private HttpRequest buildRequestByCoordinates(double lat, double lon) {
+        String url = String.format("%s?lat=%f&lon=%f&appid=%s", BASE_URL, lat, lon, API_KEY);
+        return HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .build();
+    }
+
+    private Optional<WeatherResponseDto> sendRequest(HttpRequest request) {
+        try {
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+            if (response.statusCode() == 200) {
+                return Optional.of(objectMapper.readValue(response.body(), WeatherResponseDto.class));
+            } else {
+                System.err.println("Error: " + response.statusCode() + " " + response.body());
+                return Optional.empty();
+            }
+        } catch (IOException | InterruptedException e) {
+            return Optional.empty();
+        }
+    }
+
+    
 }
