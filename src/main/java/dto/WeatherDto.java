6844e9fca2e4b0848b0f8136f744ed92f969edc8
PavelOkhrimchuk <okhrimchuk.pavel2005@gmail.com>
2024-08-21 20:37:26 +0300

    feat: WeatherDto implementation

---
+++ src/main/java/dto/WeatherDto.java
@@ -0,0 +1,25 @@
+package dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class WeatherDto {
+
+    @JsonProperty("id")
+    private int id;
+
+    @JsonProperty("main")
+    private String main;
+
+    @JsonProperty("description")
+    private String description;
+
+    @JsonProperty("icon")
+    private String icon;
+}
