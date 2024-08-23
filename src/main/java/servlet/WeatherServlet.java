d81ca9e2ee0a21e454399fdcbfe027c360034c93
PavelOkhrimchuk <okhrimchuk.pavel2005@gmail.com>
2024-08-23 23:11:07 +0300

    feat: weather servlet first implementation

---
+++ src/main/java/servlet/WeatherServlet.java
@@ -0,0 +1,64 @@
+package servlet;
+
+import dto.WeatherResponseDto;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.Location;
+import org.thymeleaf.context.WebContext;
+import repository.LocationRepository;
+import service.LocationService;
+import service.WeatherService;
+import util.ContextUtil;
+import util.HibernateUtil;
+
+import java.io.IOException;
+import java.util.Optional;
+@WebServlet("/locations/weather")
+public class WeatherServlet  extends BaseServlet{
+
+        private LocationService locationService;
+
+        @Override
+        public void init() throws ServletException {
+            super.init();
+            this.locationService = new LocationService(new LocationRepository(HibernateUtil.getSessionFactory()), new WeatherService());
+        }
+
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            String locationIdStr = req.getParameter("id");
+
+            if (locationIdStr == null) {
+                resp.sendRedirect(req.getContextPath() + "/locations");
+                return;
+            }
+
+            try {
+                int locationId = Integer.parseInt(locationIdStr);
+                Optional<Location> locationOpt = locationService.getLocationById(locationId);
+
+                if (locationOpt.isPresent()) {
+                    Location location = locationOpt.get();
+                    Optional<WeatherResponseDto> weatherOpt = locationService.getWeatherForLocation(location);
+
+                    if (weatherOpt.isPresent()) {
+                        WeatherResponseDto weather = weatherOpt.get();
+                        WebContext context = ContextUtil.buildWebContext(req, resp, getServletContext());
+                        context.setVariable("weather", weather);
+                        context.setVariable("location", location);
+                        templateEngine.process("weather.html", context, resp.getWriter());
+                    } else {
+                        resp.sendRedirect(req.getContextPath() + "/locations");
+                    }
+                } else {
+                    resp.sendRedirect(req.getContextPath() + "/locations");
+                }
+            } catch (NumberFormatException e) {
+                resp.sendRedirect(req.getContextPath() + "/locations");
+            }
+        }
+    }
+
+
