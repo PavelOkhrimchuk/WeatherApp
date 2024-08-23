1f0e199ee223a28bfbf8d469fe89b114b136d109
PavelOkhrimchuk <okhrimchuk.pavel2005@gmail.com>
2024-08-23 22:11:34 +0300

    refactor: changed fetch type

--- src/main/java/model/Location.java
+++ src/main/java/model/Location.java
@@ -25,7 +25,7 @@ public class Location {
     @Column(nullable = false)
     private Double longitude;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
